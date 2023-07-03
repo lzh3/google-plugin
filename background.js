040f6a17fabfcced42f3d121b4b22fff4b736e92
l_z_h <13417469911@qq.com>
2023-07-04 01:27:38 +0800

    插件修改中

--- background.js
+++ background.js
@@ -1,147 +1,175 @@
+var reqUrl = "http://localhost:8080/content";
 
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log("bg.js-04  line", request);
+  if (request.greeting == "crawInfo") {
+    sendContent(reqUrl);
+  }
 
-var reqUrl = 'http://localhost:8080/content';
-var isClosePage = false;
-var isCrawl = false;
-var isBlockMedia = false;
-var detailsCallback = function(details) {return { cancel: true};};
-var filter = {
-		urls: ["*://*/*"],
-		types: ["font", "image", "media"]
-	};
-var opt_extraInfoSpec = ["blocking"];
-chrome.storage.sync.get(['isCrawl', 'isClose', 'reqUrl','blockMedia'], function (result) {
-	console.log("settting is get  ", result);
-	if (result.isCrawl == true) {
-		isCrawl = true;
-		console.log("isCrawl", isCrawl);
-		if (result.reqUrl) {
-			reqUrl = result.reqUrl;
-			console.log("reqUrl", reqUrl);
-		}
-	}
-	if (result.isClose == true) {
-		isClosePage = true;
-		console.log("isClosePage", isClosePage);
-	}
-	if (result.blockMedia == true) {
-		isBlockMedia = true;
-		console.log("isBlockMedia", isBlockMedia);
-		chrome.webRequest.onBeforeRequest.addListener(
-			detailsCallback,
-			filter,
-			opt_extraInfoSpec);
-	}
+  chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+		console.log('chrome.webRequest.onBeforeRequest---bg.js', details)
+    //   return { cancel: true };
+    },
+    {
+      urls: ["*://*/*"],
+      types: ["font", "image", "media"],
+    },
+    ["blocking"]
+  );
+
+  //   sendResponse({ farewell: "goodbye" });
 });
 
-chrome.webNavigation.onCompleted.addListener(function (details) {
-	if (details.frameId == 0 && details.parentFrameId === -1) {
-		if (isCrawl) {
-			chrome.tabs.sendMessage(details.tabId, "getContent", null, (result) => {
-				var param = { "content": result.content };
-				sendContent(reqUrl, param, details.tabId);
-			})
-		}
-		if (isClosePage) {
-			var tab = new Array(1);
-			tab[0] = details.tabId;
-			chrome.tabs.remove(tab);
-		}
-	}
+// var isClosePage = false;
+// var isCrawl = false;
+// var isBlockMedia = false;
+// var detailsCallback = function(details) {return { cancel: true};};
+// var filter = {
+// 		urls: ["*://*/*"],
+// 		types: ["font", "image", "media"]
+// 	};
+// var opt_extraInfoSpec = ["blocking"];
+// chrome.storage.sync.get(['isCrawl', 'isClose', 'reqUrl','blockMedia'], function (result) {
+// 	console.log("settting is get  ", result);
+// 	if (result.isCrawl == true) {
+// 		isCrawl = true;
+// 		console.log("isCrawl", isCrawl);
+// 		if (result.reqUrl) {
+// 			reqUrl = result.reqUrl;
+// 			console.log("reqUrl", reqUrl);
+// 		}
+// 	}
+// 	if (result.isClose == true) {
+// 		isClosePage = true;
+// 		console.log("isClosePage", isClosePage);
+// 	}
+// 	if (result.blockMedia == true) {
+// 		isBlockMedia = true;
+// 		console.log("isBlockMedia", isBlockMedia);
+// 		chrome.webRequest.onBeforeRequest.addListener(
+// 			detailsCallback,
+// 			filter,
+// 			opt_extraInfoSpec);
+// 	}
+// });
 
-})
+// chrome.webNavigation.onCompleted.addListener(function (details) {
+// 	if (details.frameId == 0 && details.parentFrameId === -1) {
+// 		if (isCrawl) {
+// 			chrome.tabs.sendMessage(details.tabId, "getContent", null, (result) => {
+// 				var param = { "content": result.content };
+// 				sendContent(reqUrl, param, details.tabId);
+// 			})
+// 		}
+// 		if (isClosePage) {
+// 			var tab = new Array(1);
+// 			tab[0] = details.tabId;
+// 			chrome.tabs.remove(tab);
+// 		}
+// 	}
 
-/**
- * 自动关闭设置
- * @param {!boolean} checked 
- */
-function autoCloseSet(checked) {
-	if (checked == true) {
-		isClosePage = true
-	} else {
-		isClosePage = false;
-	}
-}
+// })
 
-/**
- * 设置后台接受参数的接口
- * @param {String} url 
- */
-function reqUrlSet(url) {
-	reqUrl = url;
-}
+// /**
+//  * 自动关闭设置
+//  * @param {!boolean} checked
+//  */
+// function autoCloseSet(checked) {
+// 	if (checked == true) {
+// 		isClosePage = true
+// 	} else {
+// 		isClosePage = false;
+// 	}
+// }
 
-/**
- * 是否爬取页面
- * @param {boolean} checked 
- */
-function isCrawlPage(checked) {
-	isCrawl = checked;
-}
+// /**
+//  * 设置后台接受参数的接口
+//  * @param {String} url
+//  */
+// function reqUrlSet(url) {
+// 	reqUrl = url;
+// }
 
-/**
- * 是否不显示图片设置
- * @param {boolean} checked 
- */
-function blockMediaSet(checked) {
-	isBlockMedia = checked;
-	console.log(chrome.webRequest.onBeforeRequest.removeListener)
-	if(!checked){
-		chrome.webRequest.onBeforeRequest.removeListener(detailsCallback);
-	}else{
-		chrome.webRequest.onBeforeRequest.addListener(
-			detailsCallback,
-			filter,
-			opt_extraInfoSpec);
-	}
-	
-}
-/**
- * 
- * @param {String} url request url
- * @param {String} param page content 
- * @param {*} sender 
- */
+// /**
+//  * 是否爬取页面
+//  * @param {boolean} checked
+//  */
+// // function isCrawlPage(checked) {
+// // 	isCrawl = checked;
+// // }
+
+// /**
+//  * 是否不显示图片设置
+//  * @param {boolean} checked
+//  */
+// function blockMediaSet(checked) {
+// 	isBlockMedia = checked;
+// 	console.log(chrome.webRequest.onBeforeRequest.removeListener)
+// 	if(!checked){
+// 		chrome.webRequest.onBeforeRequest.removeListener(detailsCallback);
+// 	}else{
+// 		chrome.webRequest.onBeforeRequest.addListener(
+// 			detailsCallback,
+// 			filter,
+// 			opt_extraInfoSpec);
+// 	}
+
+// }
+// /**
+//  *
+//  * @param {String} url request url
+//  * @param {String} param page content
+//  * @param {*} sender
+//  */
 function sendContent(url, param, tabId) {
-	var xmlhttp = null;
-	if (window.XMLHttpRequest) {// code for all new browsers
-		xmlhttp = new XMLHttpRequest();
-	} else if (window.ActiveXObject) {// code for IE5 and IE6
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	if (xmlhttp != null) {
-		xmlhttp.open("POST", url, true);
-		xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
-		let querySring = getQueryString(param);
-		xmlhttp.send(querySring);
-	} else {
-		console.log("Your browser does not support XMLHTTP.");
-	}
+  var xmlhttp = null;
+  if (window.XMLHttpRequest) {
+    // code for all new browsers
+    xmlhttp = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    // code for IE5 and IE6
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  if (xmlhttp != null) {
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader(
+      "Content-Type",
+      "application/x-www-form-urlencoded;charset=utf-8"
+    );
+    // let querySring = getQueryString(param);
+    xmlhttp.send({
+      a: 1,
+      b: 2,
+      c: "lzh",
+    });
+  } else {
+    console.log("Your browser does not support XMLHTTP.");
+  }
 }
 
-/**
- * 
- * @param {any} paramObj 
- */
-function getQueryString(paramObj) {
-	if (typeof paramObj == 'object') {
-		let keys = Object.keys(paramObj);
-		let querySring = '';
-		for (let i = 0; i < keys.length; i++) {
-			let key = keys[i];
-			if (i == 0) {
-				//encodeURIComponent(key) + '=' + encodeURIComponent(val)
-				querySring = encodeURIComponent(key) + '=' + encodeURIComponent(paramObj[key]);
-			} else {
-				querySring = '&' + encodeURIComponent(key) + '=' + encodeURIComponent(paramObj[key]);
-			}
-		}
-		return querySring;
-	} else if (typeof paramObj === 'string') {
-		return paramObj;
-	} else {
-		return null;
-	}
+// /**
+//  *
+//  * @param {any} paramObj
+//  */
+// function getQueryString(paramObj) {
+// 	if (typeof paramObj == 'object') {
+// 		let keys = Object.keys(paramObj);
+// 		let querySring = '';
+// 		for (let i = 0; i < keys.length; i++) {
+// 			let key = keys[i];
+// 			if (i == 0) {
+// 				//encodeURIComponent(key) + '=' + encodeURIComponent(val)
+// 				querySring = encodeURIComponent(key) + '=' + encodeURIComponent(paramObj[key]);
+// 			} else {
+// 				querySring = '&' + encodeURIComponent(key) + '=' + encodeURIComponent(paramObj[key]);
+// 			}
+// 		}
+// 		return querySring;
+// 	} else if (typeof paramObj === 'string') {
+// 		return paramObj;
+// 	} else {
+// 		return null;
+// 	}
 
-}
-
+// }
--- popup.js
+++ popup.js
@@ -5,99 +5,107 @@
 
 window.onload = function () {
 
-	document.getElementById('todo-01').addEventListener('click', function () {
-		let todo01 = document.getElementById("todo-01");
-		let div01 = document.getElementById("div-01");
-		if (todo01.checked) {
-			var bg = chrome.extension.getBackgroundPage();
-			bg.isCrawlPage(true);
-			div01.style.display = "inline";
-			chrome.storage.sync.set({ isCrawl: true }, function () {
-				console.log("isCrawl is set to ", true)
-			})
-		} else {
-			var bg = chrome.extension.getBackgroundPage();
-			bg.isCrawlPage(false);
-			div01.style.display = "none";
-			chrome.storage.sync.set({ isCrawl: false }, function () {
-				console.log("isCrawl is set to ", false)
-			})
-		}
+	document.getElementById('info-btn').addEventListener('click', function(){
+		console.log('od-pc-layout-two-columns', document.getElementsByClassName('od-pc-layout-two-columns')[0])
+	})
+
+	chrome.runtime.sendMessage({greeting: "crawInfo"}, function(response) {
+		console.log(response.farewell);
 	});
 
+	// document.getElementById('todo-01').addEventListener('click', function () {
+	// 	let todo01 = document.getElementById("todo-01");
+	// 	let div01 = document.getElementById("div-01");
+	// 	if (todo01.checked) {
+	// 		var bg = chrome.extension.getBackgroundPage();
+	// 		bg.isCrawlPage(true);
+	// 		div01.style.display = "inline";
+	// 		chrome.storage.sync.set({ isCrawl: true }, function () {
+	// 			console.log("isCrawl is set to ", true)
+	// 		})
+	// 	} else {
+	// 		var bg = chrome.extension.getBackgroundPage();
+	// 		bg.isCrawlPage(false);
+	// 		div01.style.display = "none";
+	// 		chrome.storage.sync.set({ isCrawl: false }, function () {
+	// 			console.log("isCrawl is set to ", false)
+	// 		})
+	// 	}
+	// });
 
-	document.getElementById('todo-02').addEventListener('click', function () {
-		let todo02 = document.getElementById("todo-02");
-		if (todo02.checked) {
-			var bg = chrome.extension.getBackgroundPage();
-			bg.autoCloseSet(true);
-			chrome.storage.sync.set({ isClose: true }, function () {
-				console.log("isClose is set to ", true);
-			});
 
-		} else {
-			var bg = chrome.extension.getBackgroundPage();
-			bg.autoCloseSet(false);
+	// document.getElementById('todo-02').addEventListener('click', function () {
+	// 	let todo02 = document.getElementById("todo-02");
+	// 	if (todo02.checked) {
+	// 		var bg = chrome.extension.getBackgroundPage();
+	// 		bg.autoCloseSet(true);
+	// 		chrome.storage.sync.set({ isClose: true }, function () {
+	// 			console.log("isClose is set to ", true);
+	// 		});
 
-			chrome.storage.sync.set({ isClose: false }, function () {
-				console.log("isClose is set to ", false);
-			});
-		}
-	});
+	// 	} else {
+	// 		var bg = chrome.extension.getBackgroundPage();
+	// 		bg.autoCloseSet(false);
+
+	// 		chrome.storage.sync.set({ isClose: false }, function () {
+	// 			console.log("isClose is set to ", false);
+	// 		});
+	// 	}
+	// });
 
 
 
-	document.getElementById('todo-03').addEventListener('blur', function () {
-		let todo03 = document.getElementById("todo-03");
-		//在返回的对象上调用background.js 里面的函数
-		var bg = chrome.extension.getBackgroundPage();
-		bg.reqUrlSet(todo03.value);
+	// document.getElementById('todo-03').addEventListener('blur', function () {
+	// 	let todo03 = document.getElementById("todo-03");
+	// 	//在返回的对象上调用background.js 里面的函数
+	// 	var bg = chrome.extension.getBackgroundPage();
+	// 	bg.reqUrlSet(todo03.value);
 
-		chrome.storage.sync.set({ reqUrl: todo03.value }, function () {
-			console.log("reqUrl is set to ", todo03.value);
-		});
-	});
+	// 	chrome.storage.sync.set({ reqUrl: todo03.value }, function () {
+	// 		console.log("reqUrl is set to ", todo03.value);
+	// 	});
+	// });
 
-	document.getElementById('todo-04').addEventListener('click', function () {
-		let todo04 = document.getElementById("todo-04");
-		if (todo04.checked) {
-			var bg = chrome.extension.getBackgroundPage();
-			bg.blockMediaSet(true);
-			chrome.storage.sync.set({ blockMedia: true }, function () {
-				console.log("blockMedia is set to ", true);
-			});
+	// document.getElementById('todo-04').addEventListener('click', function () {
+	// 	let todo04 = document.getElementById("todo-04");
+	// 	if (todo04.checked) {
+	// 		var bg = chrome.extension.getBackgroundPage();
+	// 		bg.blockMediaSet(true);
+	// 		chrome.storage.sync.set({ blockMedia: true }, function () {
+	// 			console.log("blockMedia is set to ", true);
+	// 		});
 
-		} else {
-			var bg = chrome.extension.getBackgroundPage();
-			bg.blockMediaSet(false);
-			chrome.storage.sync.set({ blockMedia: false }, function () {
-				console.log("blockMedia is set to ", false);
-			});
-		}
-	});
+	// 	} else {
+	// 		var bg = chrome.extension.getBackgroundPage();
+	// 		bg.blockMediaSet(false);
+	// 		chrome.storage.sync.set({ blockMedia: false }, function () {
+	// 			console.log("blockMedia is set to ", false);
+	// 		});
+	// 	}
+	// });
 
-	chrome.storage.sync.get(['isCrawl', 'isClose', 'reqUrl','blockMedia'], function (result) {
-		console.log("settting is get  ", result);
-		if (result.isCrawl == true) {
-			let todo01 = document.getElementById('todo-01');
-			todo01.checked = true;
-			let div01 = document.getElementById('div-01');
-			div01.style.display = "inline"
-			if (result.reqUrl) {
-				let todo03 = document.getElementById("todo-03");
-				todo03.value = result.reqUrl;
-			}
-			//在返回的对象上调用background.js 里面的函数
-		}
-		if (result.isClose == true) {
-			let todo02 = document.getElementById('todo-02');
-			todo02.checked = true;
-		}
-		if (result.blockMedia == true) {
-			let todo04 = document.getElementById('todo-04');
-			todo04.checked = true;
-		}
-	});
+	// chrome.storage.sync.get(['isCrawl', 'isClose', 'reqUrl','blockMedia'], function (result) {
+	// 	console.log("settting is get  ", result);
+	// 	if (result.isCrawl == true) {
+	// 		let todo01 = document.getElementById('todo-01');
+	// 		todo01.checked = true;
+	// 		let div01 = document.getElementById('div-01');
+	// 		div01.style.display = "inline"
+	// 		if (result.reqUrl) {
+	// 			let todo03 = document.getElementById("todo-03");
+	// 			todo03.value = result.reqUrl;
+	// 		}
+	// 		//在返回的对象上调用background.js 里面的函数
+	// 	}
+	// 	if (result.isClose == true) {
+	// 		let todo02 = document.getElementById('todo-02');
+	// 		todo02.checked = true;
+	// 	}
+	// 	if (result.blockMedia == true) {
+	// 		let todo04 = document.getElementById('todo-04');
+	// 		todo04.checked = true;
+	// 	}
+	// });
 
 }
 
